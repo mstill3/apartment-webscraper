027fade2280bf1fad3237c30b33a6b4c4a16d768
Matt Stillwell <stillwell006@gmail.com>
2022-05-24 14:12:25 -0400

    :see_no_evil: Hide away config files

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
+import 'dotenv/config';
 import cheerio from 'cheerio';
 import puppeteer from 'puppeteer';
 import nodemailer from 'nodemailer';
 
+require('dotenv').config();
 
 // URL of the page we want to scrape
-const URL = "https://www.wexley100.com/models";
-const EMAILS = ["email@gmail.com"];
+const URL = process.env.WEB_URL!;
 
-const DESIRED_NUM_BEDS = 2;
-const DESIRED_NUM_BATHS = 2;
+const DESIRED_NUM_BEDS = process.env.DESIRED_NUM_BEDS!;
+const DESIRED_NUM_BATHS = process.env.DESIRED_NUM_BATHS!;
 
 interface Apartment {
 	name: string;
@@ -24,70 +25,72 @@ interface Apartment {
 	term?: string;
 }
 
-(async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(URL, { waitUntil: 'networkidle2' });
-	await page.screenshot({ path: 'page.png' });
+const main = () => {
+	(async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(URL, {waitUntil: 'networkidle2'});
+		await page.screenshot({path: 'page.png'});
 
-	const html = await page.evaluate(() => document.body.innerHTML);
-	const $ = cheerio.load(html);
+		const html = await page.evaluate(() => document.body.innerHTML);
+		const $ = cheerio.load(html);
 
-	const models = $('div .model-info');
-	models.each((index, model) => {
+		const models = $('div .model-info');
+		models.each((index, model) => {
 
-		const title = $(model).find('h4').text().trim();
-		const numBeds = parseInt(title.substring(0, title.indexOf(' ')));
-		const numBaths = parseInt(title.substring(title.indexOf('| ') + 1, title.lastIndexOf(' ')));
+			const title = $(model).find('h4').text().trim();
+			const numBeds = parseInt(title.substring(0, title.indexOf(' ')));
+			const numBaths = parseInt(title.substring(title.indexOf('| ') + 1, title.lastIndexOf(' ')));
 
-		const available = $(model).find('div .model-unavailable').text().trim() === '';
-		const availabilityMessage = !available ? undefined : $(model).find('div .model-available').text().trim();
+			const available = $(model).find('div .model-unavailable').text().trim() === '';
+			const availabilityMessage = !available ? undefined : $(model).find('div .model-available').text().trim();
 
-		const unit = $($(model).find('div .model-subtitle')[0]).text().trim();
+			const unit = $($(model).find('div .model-subtitle')[0]).text().trim();
 
-		const sqft = parseInt(
-			$(model).find('div .sqft').text()
-				.replace('Sq. Ft.', '')
-				.replace('\n', '')
-				.replace(',', '')
-				.replace('SqFt', '')
-				.trim()
-		);
+			const sqft = parseInt(
+				$(model).find('div .sqft').text()
+					.replace('Sq. Ft.', '')
+					.replace('\n', '')
+					.replace(',', '')
+					.replace('SqFt', '')
+					.trim()
+			);
 
-		const rent = !available ? undefined : parseInt($(model).find('div .rent').text()
-			.replace('Rent \n', '')
-			.replace('$', '')
-			.replace(',', '').trim());
-		const availableDate = !available ? undefined : $(model).find('div .available').text()
-			.replace('Available \n', '').trim();
-		const floor = !available ? undefined : $($(model).find('div .floor')).text()
-			.replace('Floor \n', '').trim();
-		const term = !available ? undefined : $($(model).find('div .term')).text()
-			.replace('Term \n', '').trim();
+			const rent = !available ? undefined : parseInt($(model).find('div .rent').text()
+				.replace('Rent \n', '')
+				.replace('$', '')
+				.replace(',', '').trim());
+			const availableDate = !available ? undefined : $(model).find('div .available').text()
+				.replace('Available \n', '').trim();
+			const floor = !available ? undefined : $($(model).find('div .floor')).text()
+				.replace('Floor \n', '').trim();
+			const term = !available ? undefined : $($(model).find('div .term')).text()
+				.replace('Term \n', '').trim();
 
-		const apartment: Apartment = {
-			name: title,
-			beds: numBeds,
-			baths: numBaths,
-			available: available,
-			availabilityMessage,
-			unit,
-			sqft,
-			rent,
-			availableDate,
-			floor,
-			term
-		}
-		console.log(apartment);
-		if (apartment.beds === DESIRED_NUM_BEDS &&
-			apartment.baths === DESIRED_NUM_BATHS &&
-			apartment.available) {
-			sendMail(apartment).catch(console.error);
-		}
-	});
+			const apartment: Apartment = {
+				name: title,
+				beds: numBeds,
+				baths: numBaths,
+				available: available,
+				availabilityMessage,
+				unit,
+				sqft,
+				rent,
+				availableDate,
+				floor,
+				term
+			}
+			console.log(apartment);
+			if (apartment.beds === DESIRED_NUM_BEDS &&
+				apartment.baths === DESIRED_NUM_BATHS &&
+				apartment.available) {
+				sendMail(apartment).catch(console.error);
+			}
+		});
 
-	await browser.close();
-})();
+		await browser.close();
+	})();
+};
 
 
 // async..await is not allowed in global scope, must use a wrapper
@@ -95,8 +98,8 @@ const sendMail = async (apartment: Apartment) => {
 	var transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-			user: 'email@gmail.com',
-			pass: 'password1!'
+			user: process.env.SENDING_EMAIL,
+			pass: process.env.SENDING_PASSWORD
 		}
 	});
 
@@ -127,7 +130,7 @@ Floor:\t\t${apartment.floor}`;
 
 	const mailOptions = {
 		from: '"Tweetie Bird 🦆" <notify@webscraper.com>',
-		to: EMAILS.join(', '),
+		to: process.env.SEND_TO_EMAILS,
 		subject: "Apartment Available!",
 		text: messageText,
 		html: messageHtml
@@ -136,3 +139,5 @@ Floor:\t\t${apartment.floor}`;
 	// send mail with defined transport object
 	await transporter.sendMail(mailOptions);
 }
+
+main();
