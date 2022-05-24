afd607141b17cbbd9f3d893fc6888f3da39be9ea
Matt Stillwell <stillwell006@gmail.com>
2022-05-23 22:50:21 -0400

    :sparkles: Add in node mail logic

--- src/app.ts
+++ src/app.ts
@@ -1,99 +1,30 @@
 import cheerio from 'cheerio';
-// import axios from 'axios';
-import pretty from 'pretty';
 import puppeteer from 'puppeteer';
-import fs from 'fs';
+import nodemailer from 'nodemailer';
 
 
 // URL of the page we want to scrape
 const URL = "https://www.wexley100.com/models";
-//
-//
-// // Async function which scrapes the data
-// async function scrapeData() {
-// 	try {
-//
-// 		// Fetch HTML of the page we want to scrape
-// 		const { data } = await axios.get(URL);
-//
-// 		// Load HTML we fetched in the previous line
-// 		const $ = cheerio.load(data);
-//
-// 		console.log(pretty($.html()));
-//
-// 		// h4  class model-title
-//
-//
-// 		// div class model-unavailable model-subtitle
-// 		// div class model-available
-//
-//
-//
-// 		// class wrap-models
-// 		// class wrap-model-item item
-//
-// 		// const models = $('.wrap-model-item div');
-// 		// models.each((index, model) => {
-// 		// 	console.log("a");
-// 		// 	// const title = $(model).children('.model-title');
-// 		// 	// const avaliablity = $(model).children('.model-title');
-// 		//
-// 		// 	// console.log(title);
-// 		// });
-//
-// 		// // Select all the list items in plainlist class
-// 		// const listItems = $(".plainlist ul li");
-// 		// // Stores data for all countries
-// 		// const countries = [];
-// 		// // Use .each method to loop through the li we selected
-// 		// listItems.each((idx, el) => {
-// 		// 	// Object holding data for each country/jurisdiction
-// 		// 	const country = { name: "", iso3: "" };
-// 		// 	// Select the text content of a and span elements
-// 		// 	// Store the textcontent in the above object
-// 		// 	country.name = $(el).children("a").text();
-// 		// 	country.iso3 = $(el).children("span").text();
-// 		// 	// Populate countries array with country data
-// 		// 	countries.push(country);
-// 		// });
-// 		// Logs countries array to the console
-// 		// console.dir(countries);
-// 		// // Write countries array in countries.json file
-// 		// fs.writeFile("coutries.json", JSON.stringify(countries, null, 2), (err) => {
-// 		// 	if (err) {
-// 		// 		console.error(err);
-// 		// 		return;
-// 		// 	}
-// 		// 	console.log("Successfully written data to file");
-// 		// });
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// }
-//
-// const main = () => {
-// 	// const $ = cheerio.load('<h2 class="title">Hello world</h2>');
-// 	//
-// 	// $('h2.title').text('Hello there!');
-// 	// $('h2').addClass('welcome');
-// 	//
-// 	// console.log(pretty($.html()));
-// 	//
-// 	// console.log('hi');
-//
-// 	// Invoke the above function
-// 	scrapeData();
-// };
-//
-//
-// main();
-//
-//
-//
+const EMAILS = ["email@gmail.com"];
 
+const DESIRED_NUM_BEDS = 2;
+const DESIRED_NUM_BATHS = 2;
+
+interface Apartment {
+	name: string;
+	beds: number;
+	baths: number;
+	available: boolean;
+	availabilityMessage?: string;
+	unit: string;
+	sqft: number;
+	rent?: number;
+	availableDate?: string;
+	floor?: string;
+	term?: string;
+}
 
 (async () => {
-	const URL = "https://www.wexley100.com/models";
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(URL, { waitUntil: 'networkidle2' });
@@ -101,44 +32,107 @@ const URL = "https://www.wexley100.com/models";
 
 	const html = await page.evaluate(() => document.body.innerHTML);
 	const $ = cheerio.load(html);
-	fs.writeFileSync('page.html', html);
-	// console.log(pretty(html));
 
-	const models = $('div .wrap-model-item');
+	const models = $('div .model-info');
 	models.each((index, model) => {
-		const title = $(model).find('h4 .model-title').text();
-		const unav = $(model).find('div .model-unavailable');
-		const av = $(model).find('div .model-available');
 
-		console.log("hello world");
-		console.log(title);
+		const title = $(model).find('h4').text().trim();
+		const numBeds = parseInt(title.substring(0, title.indexOf(' ')));
+		const numBaths = parseInt(title.substring(title.indexOf('| ') + 1, title.lastIndexOf(' ')));
+
+		const available = $(model).find('div .model-unavailable').text().trim() === '';
+		const availabilityMessage = !available ? undefined : $(model).find('div .model-available').text().trim();
+
+		const unit = $($(model).find('div .model-subtitle')[0]).text().trim();
+
+		const sqft = parseInt(
+			$(model).find('div .sqft').text()
+				.replace('Sq. Ft.', '')
+				.replace('\n', '')
+				.replace(',', '')
+				.replace('SqFt', '')
+				.trim()
+		);
+
+		const rent = !available ? undefined : parseInt($(model).find('div .rent').text()
+			.replace('Rent \n', '')
+			.replace('$', '')
+			.replace(',', '').trim());
+		const availableDate = !available ? undefined : $(model).find('div .available').text()
+			.replace('Available \n', '').trim();
+		const floor = !available ? undefined : $($(model).find('div .floor')).text()
+			.replace('Floor \n', '').trim();
+		const term = !available ? undefined : $($(model).find('div .term')).text()
+			.replace('Term \n', '').trim();
+
+		const apartment: Apartment = {
+			name: title,
+			beds: numBeds,
+			baths: numBaths,
+			available: available,
+			availabilityMessage,
+			unit,
+			sqft,
+			rent,
+			availableDate,
+			floor,
+			term
+		}
+		console.log(apartment);
+		if (apartment.beds === DESIRED_NUM_BEDS &&
+			apartment.baths === DESIRED_NUM_BATHS &&
+			apartment.available) {
+			sendMail(apartment).catch(console.error);
+		}
 	});
 
 	await browser.close();
 })();
 
 
+// async..await is not allowed in global scope, must use a wrapper
+const sendMail = async (apartment: Apartment) => {
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'email@gmail.com',
+			pass: 'password1!'
+		}
+	});
 
-// (async () => {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-//
-// 	try {
-// 		await page.goto(URL, { timeout: 180000 });
-// 		let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-// 		let $ = cheerio.load(bodyHTML);
-// 		console.log(pretty(bodyHTML));
-// 		fs.writeFileSync('data.html', bodyHTML);
-//
-// 		let models = $('.wrap-model-item div')
-// 		models.each((index, model) => {
-// 			const title = $(model).find('.model-title').text();
-// 			console.log(title);
-// 		});
-//
-// 	}  catch(err) {
-// 		console.log(err);
-// 	}
-//
-// 	await browser.close();
-// })();
+	// Create our number formatter.
+	const commaNumberFormat = new Intl.NumberFormat('en-US')
+
+	// @ts-ignore
+	const messageText = `
+Unit:\t\t\t\t${apartment.unit}
+Square Feet:\t\t${commaNumberFormat.format(apartment.sqft)}
+Rent:\t\t\t$${commaNumberFormat.format(apartment.rent || 0)}
+Number of beds:\t${apartment.beds}
+Number of baths:\t${apartment.baths}
+Date:\t\t\t${apartment.availableDate}
+Term:\t\t\t${apartment.term}
+Floor:\t\t${apartment.floor}`;
+
+	// @ts-ignore
+	const messageHtml = `
+	<strong>Unit</strong>: ${apartment.unit}<br/>
+	<strong>Square Feet</strong>: ${commaNumberFormat.format(apartment.sqft)}<br/>
+	<strong>Rent</strong>: $${commaNumberFormat.format(apartment.rent || 0)}<br/>
+	<strong>Number of beds</strong>: ${apartment.beds}<br/>
+	<strong>Number of baths</strong>: ${apartment.baths}<br/>
+	<strong>Date</strong>: ${apartment.availableDate}<br/>
+	<strong>Term</strong>: ${apartment.term}<br/>
+	<strong>Floor</strong>: ${apartment.floor}`;
+
+	const mailOptions = {
+		from: '"Tweetie Bird 🦆" <notify@webscraper.com>',
+		to: EMAILS.join(', '),
+		subject: "Apartment Available!",
+		text: messageText,
+		html: messageHtml
+	};
+
+	// send mail with defined transport object
+	await transporter.sendMail(mailOptions);
+}
