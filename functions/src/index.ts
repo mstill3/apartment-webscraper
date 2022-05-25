fccc2341fc11866c69c80a46fcb819d7ad9f0bb4
Matt Stillwell <stillwell006@gmail.com>
2022-05-25 00:55:14 -0400

    :zap: Switch up chrome version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,77 +27,76 @@ interface Apartment {
     term?: string;
 }
 
-const main = () => {
-  (async () => {
-    const browser = await puppeteer.launch({args: ["--no-sandbox"]});
-    const page = await browser.newPage();
-    await page.goto(URL, {waitUntil: "networkidle2"});
-    // await page.screenshot({path: 'page.png'});
 
-    const html = await page.evaluate(() => document.body.innerHTML);
-    const $ = cheerio.load(html);
+const main = async () => {
+  const browser = await puppeteer.launch({args: ["--no-sandbox"]});
+  const page = await browser.newPage();
+  await page.goto(URL, {waitUntil: "networkidle2"});
+  // await page.screenshot({path: 'page.png'});
 
-    const models = $("div .model-info");
-    models.each((index, model) => {
-      const title = $(model).find("h4").text().trim();
-      const numBeds = parseInt(title.substring(0, title.indexOf(" ")));
-      const numBaths = parseInt(title.substring(title.indexOf("| ") + 1,
-          title.lastIndexOf(" ")));
+  const html = await page.evaluate(() => document.body.innerHTML);
+  const $ = cheerio.load(html);
 
-      const available = $(model).find("div .model-unavailable").text()
-          .trim() === "";
-      const availabilityMessage = !available ? undefined : $(model)
-          .find("div .model-available").text().trim();
+  const models = $("div .model-info");
+  models.each((index, model) => {
+    const title = $(model).find("h4").text().trim();
+    const numBeds = parseInt(title.substring(0, title.indexOf(" ")));
+    const numBaths = parseInt(title.substring(title.indexOf("| ") + 1,
+        title.lastIndexOf(" ")));
 
-      const unit = $($(model).find("div .model-subtitle")[0]).text().trim();
+    const available = $(model).find("div .model-unavailable").text()
+        .trim() === "";
+    const availabilityMessage = !available ? undefined : $(model)
+        .find("div .model-available").text().trim();
 
-      const sqft = parseInt(
-          $(model).find("div .sqft").text()
-              .replace("Sq. Ft.", "")
-              .replace("\n", "")
-              .replace(",", "")
-              .replace("SqFt", "")
-              .trim()
-      );
+    const unit = $($(model).find("div .model-subtitle")[0]).text().trim();
 
-      const rent = !available ? undefined : parseInt($(model)
-          .find("div .rent").text()
-          .replace("Rent \n", "")
-          .replace("$", "")
-          .replace(",", "").trim());
-      const availableDate = !available ? undefined : $(model)
-          .find("div .available").text()
-          .replace("Available \n", "").trim();
-      const floor = !available ? undefined : $($(model)
-          .find("div .floor")).text()
-          .replace("Floor \n", "").trim();
-      const term = !available ? undefined : $($(model)
-          .find("div .term")).text()
-          .replace("Term \n", "").trim();
+    const sqft = parseInt(
+        $(model).find("div .sqft").text()
+            .replace("Sq. Ft.", "")
+            .replace("\n", "")
+            .replace(",", "")
+            .replace("SqFt", "")
+            .trim()
+    );
 
-      const apartment: Apartment = {
-        name: title,
-        beds: numBeds,
-        baths: numBaths,
-        available: available,
-        availabilityMessage,
-        unit,
-        sqft,
-        rent,
-        availableDate,
-        floor,
-        term,
-      };
-      console.log(apartment);
-      if (apartment.beds === parseInt(DESIRED_NUM_BEDS) &&
-                apartment.baths === parseInt(DESIRED_NUM_BATHS) &&
-                apartment.available) {
-        sendMail(apartment).catch(console.error);
-      }
-    });
+    const rent = !available ? undefined : parseInt($(model)
+        .find("div .rent").text()
+        .replace("Rent \n", "")
+        .replace("$", "")
+        .replace(",", "").trim());
+    const availableDate = !available ? undefined : $(model)
+        .find("div .available").text()
+        .replace("Available \n", "").trim();
+    const floor = !available ? undefined : $($(model)
+        .find("div .floor")).text()
+        .replace("Floor \n", "").trim();
+    const term = !available ? undefined : $($(model)
+        .find("div .term")).text()
+        .replace("Term \n", "").trim();
 
-    await browser.close();
-  })();
+    const apartment: Apartment = {
+      name: title,
+      beds: numBeds,
+      baths: numBaths,
+      available: available,
+      availabilityMessage,
+      unit,
+      sqft,
+      rent,
+      availableDate,
+      floor,
+      term,
+    };
+    console.log(apartment);
+    if (apartment.beds === parseInt(DESIRED_NUM_BEDS) &&
+            apartment.baths === parseInt(DESIRED_NUM_BATHS) &&
+            apartment.available) {
+      sendMail(apartment).catch(console.error);
+    }
+  });
+
+  await browser.close();
 };
 
 
@@ -136,7 +135,7 @@ Floor:\t\t${apartment.floor}`;
 
   const mailOptions = {
     from: "\"Tweetie Bird 🦆\" <notify@webscraper.com>",
-    to: process.env.SEND_TO_EMAILS,
+    to: (process.env.SEND_TO_EMAILS || "").replace(",", ", "),
     subject: "Apartment Available!",
     text: messageText,
     html: messageHtml,
@@ -154,6 +153,7 @@ export const webscrape = GCP.runWith({
 }).pubsub.topic(TOPIC)
     .onPublish((message: GCP.pubsub.Message) => {
       main();
+      return 0;
     });
 
 main();
